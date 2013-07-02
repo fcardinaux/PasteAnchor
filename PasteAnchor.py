3a7bd5b9a923002294d1d9b823cd9070c623a497
François Cardinaux <fcardinaux@gmail.com>
2013-07-02 06:49:50 +0200

    FIXED: PasteAnchor wasn't loaded at startup because a package was imported too early

--- PasteAnchor.py
+++ PasteAnchor.py
@@ -3,10 +3,23 @@
 # Author: Francois Cardinaux
 # Date: 2013-06-14
 import sublime, sublime_plugin, urllib, re, os.path
-from bs4 import BeautifulSoup
 
 class Paste_anchorCommand(sublime_plugin.TextCommand):
+
   def run(self, edit):
+    if not hasattr(self, 'BeautifulSoup'):
+      # Importing BeautifulSoup at the beginning of the script prevents this package from being loaded.
+      # This is very probably because dependency constraints cannot be expressed, at least with the current version
+      # of Sublime Text or Package Control.
+      # This solution is from: http://stackoverflow.com/a/4481762
+      try:
+        from bs4 import BeautifulSoup
+      except ImportError:
+        self.BeautifulSoup = None
+      else:
+        self.BeautifulSoup = BeautifulSoup
+
+    print "running command now..."
     selectedRegion = self.view.sel()[0]
     assumedUrl = sublime.get_clipboard().strip()
 
@@ -27,45 +40,54 @@ class Paste_anchorCommand(sublime_plugin.TextCommand):
         # Get the BeautifulSoup object
         soup = self.serveTheSoup(assumedUrl)
 
-        # Build the Markdown URL
-        title = soup.title.string.strip()
+        if not soup:
 
-        # Find out if HN-like site
-        isHNLike = False
-        re_comp_externalURL = re.compile('^https?://')
-        for site in config["hn-like-sites"]:
+          anchor = "You should install the BeautifulSoup package"
 
-          if None == re.match(site["url-regexp"], assumedUrl):
-            continue
+        else:
 
-          mainUrl = soup.select(site['title-anchor-selector'])[0]['href'].strip()
-          if None == re_comp_externalURL.match(mainUrl):
-            # A local link
+          # Build the Markdown URL
+          title = soup.title.string.strip()
+
+          # Find out if HN-like site
+          isHNLike = False
+          re_comp_externalURL = re.compile('^https?://')
+          for site in config["hn-like-sites"]:
+
+            if None == re.match(site["url-regexp"], assumedUrl):
+              continue
+
+            mainUrl = soup.select(site['title-anchor-selector'])[0]['href'].strip()
+            if None == re_comp_externalURL.match(mainUrl):
+              # A local link
+              anchor = anchorSyntax.format(assumedUrl, title)
+
+            else:
+              # An external link
+              mainSoup = self.serveTheSoup(mainUrl)
+              title = mainSoup.title.string.strip()
+
+              target = anchorSyntax.format(mainUrl, title)
+              source = anchorSyntax.format(assumedUrl, site['site-name'])
+              fullLink = config["hn-like-site-syntax"].format(target, source)
+              anchor = fullLink
+
+            isHNLike = True
+            break
+
+          if not isHNLike:
             anchor = anchorSyntax.format(assumedUrl, title)
 
-          else:
-            # An external link
-            mainSoup = self.serveTheSoup(mainUrl)
-            title = mainSoup.title.string.strip()
-
-            target = anchorSyntax.format(mainUrl, title)
-            source = anchorSyntax.format(assumedUrl, site['site-name'])
-            fullLink = config["hn-like-site-syntax"].format(target, source)
-            anchor = fullLink
-
-          isHNLike = True
-          break
-
-        if not isHNLike:
-          anchor = anchorSyntax.format(assumedUrl, title)
-
     except Exception, excp:
-      # print str(excp)
+      print str(excp)
       anchor = assumedUrl
 
     self.view.replace(edit, selectedRegion, anchor)
 
   # Load the specified URL and return the BeautifulSoup object
   def serveTheSoup(self, url):
-      doc = urllib.urlopen(url)
-      return BeautifulSoup(doc)
+    if not self.BeautifulSoup:
+      return False
+
+    doc = urllib.urlopen(url)
+    return self.BeautifulSoup(doc)
